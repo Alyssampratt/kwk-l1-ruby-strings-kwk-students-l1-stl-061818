b9205b0745ae550e94658f414e79fd6939fe7033
Alyssampratt <magoopratt@gmail.com>
2018-06-18 21:18:05 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -5,8 +5,8 @@ time = "6 pm"
 host_name = "harry potter"
 puts "Hello dear #{guest_name},
 
-You are incited to #{party_name} on #{date} please rsvp no later than october 30.
+You are incited to #{party_name} on #{date} please rsvp no later than october 30."
 
-Sincerely
+Sincerely"
 
-#{host_name}
+"#{host_name}"
