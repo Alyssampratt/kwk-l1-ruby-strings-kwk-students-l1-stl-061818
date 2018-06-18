a75bb6adf1c6cb31c966b37a63e8e24fe3018148
Alyssampratt <magoopratt@gmail.com>
2018-06-18 21:18:58 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,10 +3,10 @@ party_name = "the best halloween party ever"
 date = "october 31"
 time = "6 pm"
 host_name = "harry potter"
-puts "Hello dear #{guest_name},
+puts "Hello dear #{guest_name},"
 
-You are incited to #{party_name} on #{date} please rsvp no later than october 30."
+puts "You are invited to #{party_name} on #{date} please rsvp no later than october 30.
 
-Sincerely"
+Sincerely,
 
-"#{host_name}"
+#{host_name}"
